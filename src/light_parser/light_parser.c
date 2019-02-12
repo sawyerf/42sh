44faf4e3a0083c77ea6735979df13e98c13907f3
Kais TLILI <kais@tlili.net>
2019-02-12 18:26:36 +0100

    light parser inner functions are static

--- src/light_parser/light_parser.c
+++ src/light_parser/light_parser.c
@@ -6,14 +6,14 @@
 /*   By: ktlili <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 14:58:34 by ktlili            #+#    #+#             */
-/*   Updated: 2019/02/12 15:47:20 by ktlili           ###   ########.fr       */
+/*   Updated: 2019/02/12 16:01:44 by ktlili           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_light_parser.h"
 
-int fill_autocomp(t_autocomplete *autocomp, t_expecting type, char *str)
+static int fill_autocomp(t_autocomplete *autocomp, t_expecting type, char *str)
 {
 	int i;
 
@@ -28,26 +28,8 @@ int fill_autocomp(t_autocomplete *autocomp, t_expecting type, char *str)
 		return (MEMERR);
 	return (0);
 }
-/*
-int do_stuff(t_token *start, t_autocomplete *autocomp)
-{
-	t_expecting expecting;
-	int			first_word;
 
-	expecting = cmd_name;
-	first_word = 1;
-	while (start->type != NEWLINE)
-	{
-		if (start->type == WORD)
-			first_word = 0;
-		else if ((start->type >= LESSAND) && (start->type <= GREAT))
-
-		start = start->next;
-		
-	}
-}
-*/
-int	is_first_word(char *line, t_token *start, t_autocomplete *autocomp)
+static int	is_first_word(char *line, t_token *start, t_autocomplete *autocomp)
 {
 	int first_word;
 	t_token *save;
@@ -75,7 +57,7 @@ int	is_first_word(char *line, t_token *start, t_autocomplete *autocomp)
 	return (0);
 }
 
-int dispatch_types(char *line, t_token *start, t_autocomplete *autocomp)
+static int dispatch_types(char *line, t_token *start, t_autocomplete *autocomp)
 {
 
 	if ((start->type >= PIPE) && (start->type <= GREAT))
@@ -86,7 +68,8 @@ int dispatch_types(char *line, t_token *start, t_autocomplete *autocomp)
 	}
 	return (is_first_word(line, start, autocomp));
 }
-
+/*
+ */
 int	ft_light_parser(char *line, t_autocomplete *autocomplete)
 {
 	t_token *tokens;
