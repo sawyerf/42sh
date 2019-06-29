c8c2a3eb5d41db5b230f93f85361a83e8d49d22d
theduv <tduval@student.42.fr>
2019-06-29 05:57:48 +0200

    normed

--- src/matching/final_step.c
+++ src/matching/final_step.c
@@ -6,7 +6,7 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 19:01:48 by tduval            #+#    #+#             */
-/*   Updated: 2019/06/29 03:40:47 by tduval           ###   ########.fr       */
+/*   Updated: 2019/06/29 05:57:38 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,61 +37,52 @@ char		*remove_bs(char *pattern)
 	while (pattern[i])
 	{
 		if (pattern[i] == '\\')
-		{
 			ft_strremoveat(pattern, i);
-			i++;
-		}
-		else
+		if (pattern[i])
 			i++;
 	}
 	return (pattern);
 }
 
-char		**final_step(t_lfiles *lst, int layer, char *pattern)
+char		*fill_tab(t_lfiles *lists[2], char *pattern, int i)
 {
-	t_lfiles	*lists[2];
-	t_lfiles	*test;
-	char		**res;
-	struct stat	buf;
-	int			i;
-	int			f;
+	char	*res;
 
-	f = pattern[ft_strlen(pattern) - 1] == '/' ? 1 : 0;
-	lists[ORIGIN] = lst;
-	test = lst;
-	while (lst && lst->layer != layer)
-		lst = lst->next;
-	i = count_width(lst, pattern);
-	lists[TMP] = lst;
-	if (!(res = (char **)ft_memalloc(sizeof(char *) * (i + 2))))
-		return (free_lst(lst));
+	if (ft_strnequ(pattern, "../", 3)
+			|| ft_strnequ(pattern, "./", 2)
+			|| ft_strnequ(pattern, "/", 1))
+	{
+		if (!(res = ft_strdup(lists[TMP]->path)))
+			return ((char *)
+					free_all(pattern, NULL, NULL, lists[ORIGIN]));
+	}
+	else
+	{
+		lists[TMP]->path = ft_strremoveat(lists[TMP]->path, 0);
+		lists[TMP]->path = ft_strremoveat(lists[TMP]->path, 0);
+		if (!(res = ft_strdup(lists[TMP]->path)))
+			return ((char *)
+					free_all(pattern, NULL, NULL, lists[ORIGIN]));
+	}
+	return (res);
+}
+
+char		**end_game(char **res, t_lfiles *lists[2], char *pattern, int i)
+{
 	if (i == 0)
 	{
 		res[i] = remove_bs(pattern);
 		free_lst(lists[ORIGIN]);
 		return (res);
 	}
-	lists[TMP] = lst;
 	i = 0;
 	while (lists[TMP])
 	{
 		if (go_last(lists[TMP]->path)[0] != '.'
 				|| go_last(pattern)[0] == '.')
 		{
-			if (ft_strnequ(pattern, "../", 3)
-					|| ft_strnequ(pattern, "./", 2)
-					|| ft_strnequ(pattern, "/", 1))
-			{
-				if (!(res[i] = ft_strdup(lists[TMP]->path)))
-					return ((char **)free_all(pattern, NULL, NULL, lists[ORIGIN]));
-			}
-			else
-			{
-				lists[TMP]->path = ft_strremoveat(lists[TMP]->path, 0);
-				lists[TMP]->path = ft_strremoveat(lists[TMP]->path, 0);
-				if (!(res[i] = ft_strdup(lists[TMP]->path)))
-					return ((char **)free_all(pattern, NULL, NULL, lists[ORIGIN]));
-			}
+			if (!(res[i] = fill_tab(lists, pattern, i)))
+				return (NULL);
 			i++;
 		}
 		lists[TMP] = lists[TMP]->next;
@@ -99,4 +90,24 @@ char		**final_step(t_lfiles *lst, int layer, char *pattern)
 	free_lst(lists[ORIGIN]);
 	ft_strdel(&pattern);
 	return (res);
+}
+
+char		**final_step(t_lfiles *lst, int layer, char *pattern)
+{
+	t_lfiles	*lists[2];
+	char		**res;
+	struct stat	buf;
+	int			i;
+	int			f;
+
+	f = pattern[ft_strlen(pattern) - 1] == '/' ? 1 : 0;
+	lists[ORIGIN] = lst;
+	while (lst && lst->layer != layer)
+		lst = lst->next;
+	i = count_width(lst, pattern);
+	lists[TMP] = lst;
+	if (!(res = (char **)ft_memalloc(sizeof(char *) * (i + 2))))
+		return (free_lst(lst));
+	lists[TMP] = lst;
+	return (end_game(res, lists, pattern, i));
 }
