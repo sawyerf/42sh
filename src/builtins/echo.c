dd1d302eb8132854999c0cd2dd6459095baf1852
Kais TLILI <kais@tlili.net>
2019-07-06 16:57:04 +0200

    fixed echo issue #12

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: apeyret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 15:16:16 by apeyret           #+#    #+#             */
-/*   Updated: 2019/04/04 13:59:24 by apeyret          ###   ########.fr       */
+/*   Updated: 2019/07/06 16:55:29 by ktlili           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	ft_echo(t_cmd_tab *cmd)
 			i = 2;
 		while (cmd->av[i] != NULL)
 		{
-			if (write(STDOUT_FILENO, cmd->av[1],
-				ft_strlen(cmd->av[1])) == -1)
+			if (write(STDOUT_FILENO, cmd->av[i],
+				ft_strlen(cmd->av[i])) == -1)
 			{
 				ft_dprintf(STDERR_FILENO, "42sh: echo: fd error\n");
 				return (1);
